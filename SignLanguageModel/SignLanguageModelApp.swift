4f6b7e68c8505f8ba02772cc494f28eedc22f296
SunilS <spsarolkar@gmail.com>
2025-12-27 16:13:58 +0530

    Initial Commit

---
+++ SignLanguageModel/SignLanguageModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignLanguageModelApp.swift
+//  SignLanguageModel
+//
+//  Created by SunilS on 27/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SignLanguageModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
