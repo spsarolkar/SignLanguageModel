9ec8f416ac95b50656902c42f0316ff4b758be74
SunilS <spsarolkar@gmail.com>
2025-12-28 13:40:08 +0530

    Added testing scheme

---
+++ SignLanguageModelTests/SignLanguageModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  SignLanguageModelTests.swift
+//  SignLanguageModelTests
+//
+//  Created by SunilS on 28/12/25.
+//
+
+import Testing
+
+struct SignLanguageModelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
